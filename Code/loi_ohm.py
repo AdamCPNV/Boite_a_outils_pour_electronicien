2d35e6eb13d2f8d26dbd1663ba9767d87e014c5c
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-06 14:51:35 +0200

    ImplementationLoiOhm

---
+++ Code/loi_ohm.py
@@ -0,0 +1,13 @@
+import trouve_resistance_E12
+
+def resistance_inserer(tension_alimentation, tension_seul_led, courant_max):
+    tension_reel = tension_alimentation - tension_seul_led
+    resistance = tension_reel / courant_max
+
+    resistance = trouve_resistance_E12.inferieur_proche_E12(resistance)[1]
+    courant_reel = tension_reel / resistance
+    puissance_dissiper = tension_alimentation **2 /resistance
+    return resistance ,courant_reel, puissance_dissiper
+
+test = resistance_inserer(10, 2, 0.025)
+print("resistance = " + str(test[0]) + " courranr réel = " + str(test[1]) + " perte = " + str(test[2]))
--- Code/trouve_resistance_E12.py
+++ Code/trouve_resistance_E12.py
@@ -1,5 +1,7 @@
 SERIE = [1, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2]
 
+
+#permet de trouvé la résistance maximal à utiliser pour etre le plus proche de la cible
 def inferieur_proche_E12(a_trouver):
     i = 1
     while True :
@@ -12,6 +14,7 @@ def inferieur_proche_E12(a_trouver):
                     return resulta, nombre * i
 
 
+#permet de trouvé la résistance minimal à utiliser pour etre le plus proche de la cible
 def supperieur_proche_E12(a_trouver):
     i = 1
     while True :
@@ -32,6 +35,8 @@ def supperieur_proche_E12(a_trouver):
             elif resulta < 1: 
                  return ancienne_difference + 1, ancien_nombre
 
+
+#Deduit quelle est la valeur la plus proche de la cible
 def valeur_proche(min,max):
      difference_minimal = 1 - min[0]
      difference_maximal = max[0] - 1
@@ -45,8 +50,4 @@ def valeur_proche(min,max):
           print("La résistance la plus proche est de : " + str(min[1]))
           return min
 
-a = inferieur_proche_E12(550)
-b = supperieur_proche_E12(550)
-
-print(valeur_proche(a,b))
 
