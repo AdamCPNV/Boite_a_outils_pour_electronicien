861473ab84cdb5b1a44bbd6ec05293bb3747d589
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-06 12:07:44 +0200

    TrouveValeurProcheSupperieur

--- Code/electronique.py
+++ Code/electronique.py
@@ -11,5 +11,26 @@ def inferieur_proche_E12(a_trouver):
             if resulta <= 1:
                     return resulta
 
-print(inferieur_proche_E12(500))
+
+def supperieur_proche_E12(a_trouver):
+    i = 1
+    while True :
+        i *= 10
+        ancienne_difference = 1
+        for nombre in SERIE:
+            resulta = a_trouver / (nombre * i)
+            print(str(a_trouver) + " diviser par " + str(nombre * i ) + " = " + str(resulta))
+            # tenos que le nombre est compris entre 1 et 2 on le divise par plus grand
+            if resulta >= 1 and resulta <= 2:
+                    difference = resulta - 1
+                    if ancienne_difference > difference:
+                         ancienne_difference = difference
+                         None
+                    else:
+                        return resulta
+            elif resulta < 1: 
+                 return ancienne_difference + 1 
+
+
+print(supperieur_proche_E12(600))
 
