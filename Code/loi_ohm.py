7449e99669998961d3f7aeee3a36cd774532e0e6
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-06 14:53:13 +0200

    CorrectionNomanclatureInverser

--- Code/loi_ohm.py
+++ Code/loi_ohm.py
@@ -4,7 +4,7 @@ def resistance_inserer(tension_alimentation, tension_seul_led, courant_max):
     tension_reel = tension_alimentation - tension_seul_led
     resistance = tension_reel / courant_max
 
-    resistance = trouve_resistance_E12.inferieur_proche_E12(resistance)[1]
+    resistance = trouve_resistance_E12.supperieur_proche_E12(resistance)[1]
     courant_reel = tension_reel / resistance
     puissance_dissiper = tension_alimentation **2 /resistance
     return resistance ,courant_reel, puissance_dissiper
--- Code/trouve_resistance_E12.py
+++ Code/trouve_resistance_E12.py
@@ -2,7 +2,7 @@ SERIE = [1, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2]
 
 
 #permet de trouvé la résistance maximal à utiliser pour etre le plus proche de la cible
-def inferieur_proche_E12(a_trouver):
+def supperieur_proche_E12(a_trouver):
     i = 1
     while True :
         i *= 10
@@ -15,7 +15,7 @@ def inferieur_proche_E12(a_trouver):
 
 
 #permet de trouvé la résistance minimal à utiliser pour etre le plus proche de la cible
-def supperieur_proche_E12(a_trouver):
+def inferieur_proche_E12(a_trouver):
     i = 1
     while True :
         i *= 10
@@ -41,7 +41,7 @@ def valeur_proche(min,max):
      difference_minimal = 1 - min[0]
      difference_maximal = max[0] - 1
 
-     if difference_minimal > difference_maximal:
+     if difference_minimal < difference_maximal:
           print("Valeur max :" + str(difference_maximal))
           print("La résistance la plus proche est de : " + str(max[1]))
           return max
