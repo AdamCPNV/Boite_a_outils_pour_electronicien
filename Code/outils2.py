12aeaf17258dfa8d6d7d055027a250ec0ae2956c
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-13 11:40:16 +0200

    outils2.1fonctionnelle

--- Code/outils2.py
+++ Code/outils2.py
@@ -2,10 +2,12 @@ import tkinter
 import trouve_produit_inserer
 
 
-def recuperer_champs(entrer_temps_charge_condensateur, entrer_capaciter_condensateur):
+def recuperer_champs( entrer_temps_charge_condensateur,  entrer_capaciter_condensateur, taille_reistance, temps_charge_reel_reistance):
     temps_charge_condensateur = float(entrer_temps_charge_condensateur.get())
     capaciter_condensateur = float(entrer_capaciter_condensateur.get())
     resltat = trouve_produit_inserer.resistance_inserer(temps_charge_condensateur, capaciter_condensateur)
+    taille_reistance.config(text = " la résitance sera de : " + str(resltat[0]))
+    temps_charge_reel_reistance.config(text = " le temps de charge réel sera de  : " + str(resltat[1]))
     print(resltat)
 
 def outils2(maitre):
@@ -22,7 +24,13 @@ def outils2(maitre):
 
     bouton_calculer = tkinter.Button(frame, text="Calculer", command = lambda :(recuperer_champs(
         entrer_temps_charge_condensateur,
-        entrer_capaciter_condensateur)))
+        entrer_capaciter_condensateur,
+        text_taille_resitance,
+        text_temps_charge_reel
+        )))
+
+    text_taille_resitance = tkinter.Label(frame, text="")
+    text_temps_charge_reel = tkinter.Label(frame, text="")
 
     bouton_resistance.grid(row = 0, column  = 0)
     bouton_condensateur.grid(row = 0, column = 1)
@@ -34,5 +42,8 @@ def outils2(maitre):
     entrer_capaciter_condensateur.grid(row = 2, column = 1)
 
     bouton_calculer.grid(row = 3, column = 0)
-    
+
+    text_taille_resitance.grid(row=4, column= 0)
+    text_temps_charge_reel.grid(row= 5, column= 0)
+
     return frame
