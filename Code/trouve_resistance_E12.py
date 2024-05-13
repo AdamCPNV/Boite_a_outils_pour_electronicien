8346a22b5f45967c9bc298919499908b2b8a5bc4
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-13 11:24:55 +0200

    ModifactionRetourTrouveResistance

--- Code/trouve_resistance_E12.py
+++ Code/trouve_resistance_E12.py
@@ -31,31 +31,34 @@ def inferieur_proche_E12(resistance):
     while True :
         i *= 10
         ancienne_difference = 1
-        for nombre in SERIE:
-            resulta = resistance / (nombre * i)
-            print(str(resistance) + " diviser par " + str(nombre * i ) + " = " + str(resulta))
-            # tanps que le nombre est compris entre 1 et 2 on le divise par plus grand
+        for resistance_E12 in SERIE:
+            resulta = resistance / (resistance_E12 * i)
+            print(str(resistance) + " diviser par " + str(resistance_E12 * i ) + " = " + str(resulta))
+            # tanps que le resistance_E12 est compris entre 1 et 2 on le divise par plus grand
+            #Ne fonctionne pas si la resitance est inferieur a 1 
             if resulta >= 1 and resulta <= 2:
                     difference = resulta - 1
                     if ancienne_difference > difference:
                          ancienne_difference = difference
-                         ancien_nombre = nombre * i
+                         ancien_resistance_E12 = resistance_E12 * i
                          None
                     else:
-                        return resulta, nombre * i
+                        return resistance_E12 * i
             elif resulta < 1: 
-                 return ancienne_difference + 1, ancien_nombre
+                 return ancien_resistance_E12
 
 #Deduit quelle est la valeur la plus proche de la cible
 def valeur_proche(min,max):
-     difference_minimal = 1 - min[0]
-     difference_maximal = max[0] - 1
+     difference_minimal = 1 - min
+     difference_maximal = max - 1
 
      if difference_minimal < difference_maximal:
           print("Valeur max :" + str(difference_maximal))
-          print("La résistance la plus proche est de : " + str(max[1]))
+          print("La résistance la plus proche est de : " + str(max))
           return max
      else:
           print("Valeur min :" + str(difference_minimal))
-          print("La résistance la plus proche est de : " + str(min[1]))
+          print("La résistance la plus proche est de : " + str(min))
           return min
+
+
