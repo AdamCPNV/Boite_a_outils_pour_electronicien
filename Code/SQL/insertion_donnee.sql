55da47870e620cf7fe1815a0bdb383d9dc875144
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-16 16:41:44 +0200

    WIP

--- Code/SQL/insertion_donnee.sql
+++ Code/SQL/insertion_donnee.sql
@@ -1,7 +1,13 @@
 use boite_a_outils_pour_electronicien;
+insert into product (Type,Value,Size,Price,`Manufacturer-reference`)
+value(1,1.1,1.2,1.3,"ref1"),
+(1,1.2,1.2,1.2,"ref2"),
+(2,1.3,1.3,1.3,"ref3");
+
+use boite_a_outils_pour_electronicien;
 
 insert into supplier (Name,Address,PhoneNumber) 
 values("Fourniseur 1","Rued du moulin 8",001112233),
 ("Fourniseur 2","Rued du moulin 8",011112233),
-("Fourniseur 3","Rued du moulin 8",021112233);
+("Fourniseur 3","Rued du moulin 8",021112123);
 
