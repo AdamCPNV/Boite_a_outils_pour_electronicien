008b9cb6f1e112c471d0c6189ec3ffb671cf4d95
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-16 14:11:47 +0200

    AvecParametreOptionel

--- Code/integration.py
+++ Code/integration.py
@@ -31,11 +31,16 @@ class IntergationDB():
         self.mydb.commit()
 
     def modification(self,numero_actuelle, nom = "Name", addresse = "Address", nouveaux_numero_telephone = "PhoneNumber"):
-        requete = "UPDATE supplier SET Name = '{}', Address = '{}', PhoneNumber = {} WHERE PhoneNumber = {};"
+
+        if nom != "Name":
+            nom = "'" + nom + "'"
+
+        requete = "UPDATE supplier SET Name = {}, Address = '{}', PhoneNumber = {} WHERE PhoneNumber = {};"
         self.mycursor.execute(requete .format(nom, addresse, nouveaux_numero_telephone, numero_actuelle ))
         self.mydb.commit()       
 
 
 test = IntergationDB()
+
 
 print(test.modification(98, addresse="yopopt"))
