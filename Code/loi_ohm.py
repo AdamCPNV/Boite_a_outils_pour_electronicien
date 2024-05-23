14ed440ab2bac03a5914976452e0d4c9dbe7e10a
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-23 16:49:41 +0200

    WIP

--- Code/loi_ohm.py
+++ Code/loi_ohm.py
@@ -11,6 +11,7 @@ PERTE = [250,500,1000,2000,5000,10000]
 
 #Calcule le taux de perte à appliquer à une résistance
 def perte(perte):
+    perte = perte * 0.001
     for nombre in PERTE:
         if nombre >= perte:
             return nombre
@@ -26,7 +27,9 @@ def resistance_inserer(tension_alimentation, tension_seul_led, courant_max):
 
     resistance = trouve_resistance_E12.supperieur_proche_E12(resistance)
     courant_reel = tension_reel / resistance
-    puissance_dissiper = tension_alimentation **2 /resistance
+    puissance_dissiper = (tension_alimentation - tension_seul_led) **2/5
     puissance_dissiper = perte(puissance_dissiper)
     return resistance ,courant_reel, puissance_dissiper
 
+
+print(resistance_inserer(5,1,1.8))
