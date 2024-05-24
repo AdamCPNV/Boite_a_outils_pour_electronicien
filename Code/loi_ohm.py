d30e52b99f8da3226b26533ddcd2e0991b5f3a4a
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-24 08:52:21 +0200

    DebugCalculePuissance

--- Code/loi_ohm.py
+++ Code/loi_ohm.py
@@ -1,18 +1,20 @@
 """
 Auteur : Adam Sifate
 Projet : Boîte à outils pour électronicien
-Version : 0.2
-Date : 21.05.2024
+Version : 0.3
+Date : 24.05.2024
 """
 import trouve_resistance_E12
 
 #perte en milliwatt
-PERTE = [250,500,1000,2000,5000,10000]
+PUISSANCE_DISIPER = [250,500,1000,2000,5000,10000]
 
 #Calcule le taux de perte à appliquer à une résistance
 def perte(perte):
-    perte = perte * 0.001
-    for nombre in PERTE:
+    perte = perte * 1000
+    if perte > PUISSANCE_DISIPER[5]:
+        return "Trop grand"
+    for nombre in PUISSANCE_DISIPER:
         if nombre >= perte:
             return nombre
 
@@ -27,9 +29,6 @@ def resistance_inserer(tension_alimentation, tension_seul_led, courant_max):
 
     resistance = trouve_resistance_E12.supperieur_proche_E12(resistance)
     courant_reel = tension_reel / resistance
-    puissance_dissiper = (tension_alimentation - tension_seul_led) **2/5
+    puissance_dissiper = (tension_alimentation - tension_seul_led) **2/resistance
     puissance_dissiper = perte(puissance_dissiper)
     return resistance ,courant_reel, puissance_dissiper
-
-
-print(resistance_inserer(5,1,1.8))
