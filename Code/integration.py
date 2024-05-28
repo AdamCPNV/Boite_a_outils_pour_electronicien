d0bc843b289bc6b980caa8916f03dd058d6ba42d
Adam Sifate <adam.sifate@eduvaud.ch>
2024-05-28 08:43:34 +0200

    WIP

--- Code/integration.py
+++ Code/integration.py
@@ -20,7 +20,7 @@ class IntergationDB():
 
     def selection(self):
 
-        requete = "select idSupplier, Name, Address, PhoneNumber from supplier;"
+        requete = "select supplier_id, Name, Address, phone_number from supplier;"
         
         self.mycursor.execute(requete)
         myresult = self.mycursor.fetchall()
@@ -28,25 +28,25 @@ class IntergationDB():
 
 
     def insertion(self,nom, addresse, numero_telephone):       
-        requete = "insert into supplier (Name,Address,PhoneNumber) value ('{}','{}','{}');"
+        requete = "insert into supplier (Name,Address,phone_number) value ('{}','{}','{}');"
         self.mycursor.execute(requete .format(nom, addresse, numero_telephone))
         self.mydb.commit()
     
     def suppertion(self, numero_telephone):
-        requete = "DELETE FROM supplier WHERE PhoneNumber = {};"
+        requete = "DELETE FROM supplier WHERE phone_number = {};"
         self.mycursor.execute(requete .format(numero_telephone))
         self.mydb.commit()
 
     def modification(self,numero_actuelle, nom, addresse, nouveaux_numero_telephone):
 
-        requete = "UPDATE supplier SET Name = {}, Address = {}, PhoneNumber = {} WHERE PhoneNumber = {};"
+        requete = "UPDATE supplier SET Name = {}, Address = {}, phone_number = {} WHERE phone_number = {};"
         self.mycursor.execute(requete .format(nom, addresse, nouveaux_numero_telephone, numero_actuelle ))
         self.mydb.commit()
 
     def rechercher_produit(self,type, valeur, taille):
-        requete = """select  product.Price, product.`Manufacturer-reference`,product.Type,  product.Value, product.Size, supplier.Name
+        requete = """select  product.Price, product.`manufacturer-reference`,product.Type,  product.Value, product.Size, supplier.Name
         from product
-        inner join product_has_supplier ON idproduct = product_id inner join supplier on idSupplier = product_has_supplier.supplier_id
+        inner join product_has_supplier ON product_id = product_id inner join supplier on supplier_id = product_has_supplier.supplier_id
         where (product.Type = {}) and (product.Value between {} and {}) and (product.Size between {} and {});"""
 
         self.mycursor.execute(requete.format(type, valeur - 0.001, valeur , taille - 0.001, taille ))
@@ -54,9 +54,9 @@ class IntergationDB():
         return myresult
     
     def rechercher_resistance(self, valeur):
-        requete = """select  product.Price, product.`Manufacturer-reference`,product.Type,  product.Value, product.Size, supplier.Name
+        requete = """select  product.Price, product.`manufacturer-reference`,product.Type,  product.Value, product.Size, supplier.Name
         from product
-        inner join product_has_supplier ON idproduct = product_id inner join supplier on idSupplier = product_has_supplier.supplier_id
+        inner join product_has_supplier ON product_id = product_id inner join supplier on supplier_id = product_has_supplier.supplier_id
         where (product.Type = 1) and (product.Value between {} and {});"""
         self.mycursor.execute(requete.format(valeur - 0.001, valeur))
         myresult = self.mycursor.fetchall()
